778c6293aadd5faf18f390c04580817f51fafbbd
sniquey <veronique.eldridge@gmail.com>
2021-06-27 16:56:01 +1000

    everything up until checkwin

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 // Declare Variables //
 const qwerty = document.getElementById('qwerty');
+const letterButtons = qwerty.getElementsByTagName('button');
 const phrase = document.getElementById('phrase');
 const startButton = document.querySelector('.btn__reset');
 const ul = document.getElementsByTagName('ul')[0];
+const ol = document.querySelector('ol');
+let match = [];
+let lettersArray = [];
 let missed = 0;
-let phrases = ['Eating ice cream on a hot day can be a good way to cool off',
+const phrases = ['Eating ice cream on a hot day can be a good way to cool off',
                 'She helped to build the roof',
                 'In the kitchen you will find my mum',
                 'Friday became a cool wet afternoon',
-                'Mary might have been waiting outside for you']
+                'Mary might have been waiting outside for you'];
 
 //return a random phrase from the array
 const getRandomPhraseAsArray = (arr) => {
-    const num = Math.floor(Math.random()*6);
+    const num = Math.floor(Math.random()*5);
     let randomPhrase = arr[num];
     return randomPhrase;
 }
@@ -32,36 +36,54 @@ const addPhraseToDisplay = (phrase) => {
             li.className = "letter";
         }
     }
+    const lettersLI = document.getElementsByClassName('letter');
+    return lettersLI;
 }
 
-if (randomPhrase) {
-    addPhraseToDisplay(randomPhrase);
-} else {
-    const randomPhrase = getRandomPhraseAsArray(phrases);
-}
+addPhraseToDisplay(randomPhrase);
 
+const lettersLI = document.querySelector('ul').children;
+
+for (let i=0;i<lettersLI.length;i++) {
+    lettersArray.push(lettersLI[i].textContent.toLowerCase());
+ }
+
+ console.log(lettersArray);
+
+function heartsDisplay(missed) {
+        ol.children[missed - 1].style.display = "none";
+}
 
 //check if a letter is in the phrase
-const checkLetter = (input) => {
-    const key = input;
-    const checkLetter = document.getElementsByClassName('letter')
-    let match = 0;
-    let lettersArray = [];
-    for (let i=0;i<checkLetter.length;i++) {
-        lettersArray.push(checkLetter[i].textContent.toLowerCase());
-    }
-    console.log(lettersArray);
-    if (lettersArray.includes(key)) {
-        match = 1;
+const checkLetter = (input) => { 
+    // for each letter in the list of letters that make up the phrase go through and check
+    for(let i=0;i<lettersLI.length;i++){
+        // if the letter is equal to the button letter pressed AND the class name of that li is "letter"
+        if (lettersLI[i].textContent.toLowerCase() === input.textContent && lettersLI[i].className === "letter") {
+            console.log(`match at position ${i}`)
+            // change the class name from "letter" to "show" to display that letter
+            lettersLI[i].className = "show";
+            // add the letter to the match array
+            match.push(lettersLI[i].textContent);
+        // if the letter is equal to the button letter pressed and this letter has already been selected
+        } else if (lettersLI[i].textContent.toLowerCase() === input.textContent && lettersLI[i].className === "show") {
+            // add keyboard button class "chosen" on the letter
+            input.className = "chosen";
+        // if the keyboard letter is not in the array of letters and has not already been selected
+        } else if (!lettersArray.includes(input.textContent) && input.className !== "chosen") {
+            input.className = "chosen";
+            missed++;
+            heartsDisplay(missed);
+            console.log(missed);
+            return missed;
+        }
     }
     console.log(match);
-    // for (let i=0;i<checkLetter.length;i++) {
-    //     if (key === parseInt(checkLetter[i]) {
-
-    //     }
-    // }
-
+    return match;
 }
+
+
+
 
 // check if the game has been won or lost
 const checkWin = () => {
@@ -77,12 +99,9 @@ startButton.addEventListener('click', () => {
 
 //listen for the onscreen keyboard to be clicked
 qwerty.addEventListener('click', (e) => {
-    const button = e.target;
-    const letterClicked = button.textContent;
-    // if (button.className !== 'keyrow') {
-    //    console.log('no');
-    // } else {
-    //     console.log(letterClicked);
-    // }
-    checkLetter(letterClicked);
+    const isButton = e.target.nodeName === 'BUTTON';
+    if(isButton) {
+        const button = e.target;
+        checkLetter(button);
+    }
 });
